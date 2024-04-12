6706aa774b84de858e4b45bb26cf5429787939d7
rhs-dnu <rhs@dnu.com.mx>
2024-04-11 22:53:08 -0600

    Endpoints nuevos

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('Hola weies del mundo!');
+    res.send('Hola weies del mundo! esta es la api principal.');
+});
+
+app.get('/saludar', function(req, res){
+    res.send('Hola a todos');
+});
+
+app.get('/despedir', function(req, res){
+    res.send('Bai geis!');
 });
 
 app.listen(3000, function(){
