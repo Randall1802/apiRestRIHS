928cd7bacc1479dddc71443e255e3c66b372e28d
rhs-dnu <rhs@dnu.com.mx>
2024-04-30 12:38:56 -0600

    Se agrega password hasheado

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,23 @@
 const { request, response } = require('express');
 const pool = require('../data/config');
 
+var sha1 = require('js-sha1');
+var { sha256, sha224 } = require('js-sha256');
+var md5 = require('md5');
+
+const encryptPasswordSHA1 = (password) => {
+    return sha1(password);
+};
+
+const encryptPasswordSHA256 = (password) => {
+    return sha256(password);
+};
+
+
+const encryptPasswordMD5 = (password) => {
+    return md5(password);
+};
+
 //ruta de la app
 const router = app => {
     //mostrar mensaje de bienvenida de root
@@ -38,6 +55,14 @@ const router = app => {
     app.post('/users', (request, response) => {
         //response.header('Access-Control-Allow-Origin','*');
 
+        const Passwordsha256 = encryptPasswordSHA256(request.body.password);
+        const Passwordsha1 = encryptPasswordSHA1(request.body.password);
+        const Passwordmd5 = encryptPasswordMD5(request.body.password); 
+        
+        request.body.passwordsha1 = Passwordsha1;
+        request.body.passwordsha256 = Passwordsha256;
+        request.body.passwordmd5 = Passwordmd5;
+
         pool.query('INSERT INTO users SET ?', request.body, (error, result) => {
             if (error) throw error;
 
@@ -49,6 +74,14 @@ const router = app => {
         //response.header('Access-Control-Allow-Origin','*');
         const id = request.params.id;
 
+        const Passwordsha256 = encryptPasswordSHA256(request.body.password);
+        const Passwordsha1 = encryptPasswordSHA1(request.body.password);
+        const Passwordmd5 = encryptPasswordMD5(request.body.password); 
+        
+        request.body.passwordsha1 = Passwordsha1;
+        request.body.passwordsha256 = Passwordsha256;
+        request.body.passwordmd5 = Passwordmd5;
+        
         pool.query('UPDATE users SET ? WHERE id = ?', [request.body, id], (error, result) => {
             if (error) throw error;
 
